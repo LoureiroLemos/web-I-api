f6c587d7db104b1fb9b9225c08e164f5e80902e3
Pedro Loureiro de Lemos <pedrotlgl@gmail.com>
2023-12-14 08:24:58 -0300

    final

--- js/scriptBusca.js
+++ js/scriptBusca.js
@@ -1,27 +1,71 @@
 const form = document.getElementById('personagem-form');
 const container = document.querySelector(".container")
 
+
+//método POST
+const createPost = async (novoPostJSON) => {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: novoPostJSON,
+    });
+
+    const data = await res.json();
+    return data;
+
+    if (!res.ok) {
+      console.log(data.description);
+      return;
+    }
+
+  } catch (error) {
+    console.log(`Erro - ${error}`);
+  }
+
+};
+
+const deletePost = async (id) => {
+  try {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, 
+      {
+      method: 'DELETE',
+      }
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+      console.log(data.description);
+      return;
+  }
+
+  console.log(data);
+
+  } catch (error) {
+  console.log(`Erro - ${error}`);
+  }
+}
+
+
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
   
-    const nome = encodeURIComponent(form.querySelector('#nome').value.trim());
-    
-  
+    const nome = form.querySelector('#nome').value.trim();
+      
     container.innerHTML = '';
   
     try {
-      const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${nome}`);
+      const res = await fetch(`https://rickandmortyapi.com/api/character/?name=${nome}`);
       
-      if (!response.ok) {
+      if (!res.ok) {
         throw new Error('Erro ao buscar personagens.');
       }
   
-      const data = await response.json();
+      const data = await res.json();
   
-      if (!data.results.length) {
-        container.innerHTML = `<p>Nenhum personagem encontrado.</p>`;
-        return;
-      }
   
       data.results.forEach((personagem) => {
         const card = document.createElement('div');
@@ -39,108 +83,77 @@ form.addEventListener('submit', async (event) => {
         h2.textContent = `Nome: ${personagem.name}`;
         conteudo.appendChild(h2);
   
-        const p1 = document.createElement('p');
-        p1.textContent = `Espécie: ${personagem.species}`;
-        conteudo.appendChild(p1);
-  
-        const p2 = document.createElement('p');
-        p2.textContent = `Status: ${personagem.status}`;
-        conteudo.appendChild(p2);
-
-        const p3 = document.createElement('p');
-        p3.textContent = `Localização: ${personagem.location.name}`;
-        conteudo.appendChild(p3);
-
         const divBtn = document.createElement('div');
         divBtn.classList.add('btn-container');
 
         const btnPost = document.createElement('button');
-        btnPost.textContent = 'POST!';
+        btnPost.textContent = 'Mostrar informações';
         btnPost.id = `btnPost-${personagem.id}`;
         divBtn.appendChild(btnPost);
-
-        const btnDelete = document.createElement('button');
-        btnDelete.textContent = 'DELETE!';
-        btnDelete.id = `btnDelete-${personagem.id}`;
-        divBtn.appendChild(btnDelete);
         
         card.appendChild(conteudo);
         card.appendChild(divBtn);
         container.appendChild(card);
 
-        btnPost.addEventListener('click', () => {
-          console.log(`clicou no personagem ${personagem.name}`);
+        btnPost.addEventListener('click', async () => {
+          console.log(`clicou em mostrar informações do personagem ${personagem.name}`);
 
-          const createPost= async (novoPostJSON) => {
-            try {
-              const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                headers: {
-                  "Content-Type": "application/json; charset=UTF-8",
-                },
-                body: novoPostJSON,
-              });
-          
-              const data = await res.json();
-              console.log(data);
-          
-              if (!res.ok) {
-                console.log(data.description);
-                return;
-              }
-          
-            } catch (error) {
-              console.log(`Erro - ${error}`);
-            }
-          }
-
-          
-
-          let novoPost ={
+          let novoPost = {
             userId: `${personagem.id}`,
-            title: `Character: ${personagem.name}`,
-            body: `This character was last seen in ${personagem.location.name}`,
-          };
+            title: `Status: ${personagem.status}`,
+            body: `Last known location: ${personagem.location.name}`,
+            };
+  
+            novoPostJSON = JSON.stringify(novoPost);
 
-          let novoPostJSON = JSON.stringify(novoPost);
+          const data = await createPost(novoPostJSON);
 
-          createPost(novoPostJSON);
+          const divPost = document.createElement('div');
+          divBtn.classList.add('div-post');
+          conteudo.appendChild(divPost);
+
+          const p1 = document.createElement('p');
+          p1.textContent = data.title;
+          divPost.appendChild(p1);
+
+          const p2 = document.createElement('p');
+          p2.textContent = data.body;
+          divPost.appendChild(p2);
+
+          btnPost.style.display = "none";
+          const btnDelete = document.createElement('button');
+          btnDelete.textContent = 'Esconder Informações';
+          btnDelete.id = `btnDelete-${personagem.id}`;
+          divBtn.appendChild(btnDelete);
+
+
+          btnDelete.addEventListener('click', async () =>{
+          console.log(`clicou em esconder informações do personagem ${personagem.name}`);
+
+          deletePost(novoPost.userId);
+
+          btnDelete.style.display="none";
+          btnPost.style.display = "";
+          divPost.innerHTML = '';
+});
         });
-
-        btnDelete.addEventListener('click', () => {
-          console.log(`clicou no personagem ${personagem.name}`);
-
-          async function deletePost(id) {
-          try {
-            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, 
-              {
-                method: 'DELETE',
-              }
-            );
-        
-            const data = await res.json();
-
-            if (!res.ok) {
-              console.log(data.description);
-              return;
-            }
-
-            console.log(data);
-        
-          } catch (error) {
-            console.log(`Erro - ${error}`);
-          }
-        }
-        let id = `${personagem.id}`
-        deletePost(id);
-        })
-
        
       });
+
     } catch (error) {
       console.error(error);
     }
 });
+
+
+
+
+
+
+
+
+
+
 
 
 
